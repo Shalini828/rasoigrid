9e20aeececd3d98efc505ce15ef20fbab36e739e
Shalini <jhashalini363@gmail.com>
2026-09-14 00:40:07 +0530

    Prediction

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,6 +15,7 @@ from app.routes.volunteer import router as volunteer_router
 from app.routes.rescue_requests import router as rescue_requests_router
 from app.models.dispatch import Dispatch
 from app.routes.dispatch import router as dispatch_router
+from app.routes.prediction import router as prediction_router
 
 app = FastAPI(title="RasoiGrid API")
 Base.metadata.create_all(bind=engine)
@@ -24,6 +25,7 @@ app.include_router(ngo_router)
 app.include_router(volunteer_router)
 app.include_router(rescue_requests_router)
 app.include_router(dispatch_router)
+app.include_router(prediction_router)
 
 
 @app.get("/")
